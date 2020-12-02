12591c67e61bcecd78a0a5704b42f2b2bf93c49c
Park Yong Ha <yonghapk@gmail.com>
2020-12-02 23:52:40 +0900

    ADD MODAL

--- js/webcomponent.js
+++ js/webcomponent.js
@@ -1,3 +1,23 @@
-var Modal = function (element, options) {
-
-}
+$(function () {
+	var Modal = {
+		modalOpen: function (ele) {
+			$(ele.data('modalopen')).show();
+		},
+		modalClose: function (ele) {
+			if (ele.data('modalclose') === 'all') {
+				$('.modal').hide();
+				return;
+			}
+			$(ele.data('modalclose')).hide();
+		},
+		bind: function () {
+			$('[data-modalopen]').bind('click', function () {
+				Modal.modalOpen($(this))
+			})
+			$('[data-modalclose]').bind('click', function () {
+				Modal.modalClose($(this))
+			})
+		}
+	}
+	Modal.bind();
+});
