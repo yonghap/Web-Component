5e6da452c50502338f782c9c7d765b7d3d78866f
Park Yong Ha <yonghapk@gmail.com>
2022-02-10 02:01:56 +0900

    add tab accordion

--- js/common.js
+++ js/common.js
@@ -1,12 +1,35 @@
-/**
- * 탭메뉴
- * @param e
- * @returns {*}
- */
-$.fn.tab = function (e) {
-	return this.each(function () {
-		var target = $(this).attr('href');
-		$(this).addClass('active').siblings().removeClass('active');
-		$(target).addClass('active').siblings().removeClass('active');
-	})
-}
+(function ($) {
+	/**
+	 * 탭,아코디언
+	 * @param e
+	 * @returns {*}
+	 */
+	$.fn.tab = function (e) {
+		return this.each(function () {
+			const type = $(this).data('toggle'),
+				target = $(this).data('target'),
+				parent = $(this).data('parent');
+
+			switch (type) {
+				case "tab" :
+					var currentTab = $(target);
+					currentTab.show();
+					$(parent).find('.tab-content').not(currentTab).hide();
+					break;
+				case "accordion" :
+					var currentTab = $(target);
+					currentTab.stop().slideDown();
+					$(parent).find('.tab-content').not(currentTab).stop().slideUp();
+					break;
+			}
+		})
+	}
+
+}(jQuery));
+
+$(function () {
+	$('[data-toggle]').click(function (e) {
+		e.preventDefault();
+		$(this).tab();
+	});
+});
